faf8b7f2412658b24151882651845a66d768b9ec
VECNA202005 <jn.gokulkrishna@gmail.com>
2025-05-07 10:23:34 +0530

    Restructure backend for deployment

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://srv-d0de631r0fns73950osg.onrender.com/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://bloodbank-gcpl.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
